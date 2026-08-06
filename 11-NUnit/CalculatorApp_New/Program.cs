147d0168ad3c1f33a1e4797c62767f8b5615e9e7
Kavya Golla <23BQ1A1238@vvit.net>
2026-08-06 14:05:15 +0530

    Added NUnit Calculator project

---
+++ 11-NUnit/CalculatorApp_New/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Programs
+{
+    static void Main()
+    {
+        Calculator1 cal = new Calculator1();
+
+        Console.Write("Enter First Number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter Second Number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Addition = " + cal.Add(a, b));
+        Console.WriteLine("Subtraction = " + cal.Subtract(a, b));
+        Console.WriteLine("Multiplication = " + cal.Multiply(a, b));
+
+        try
+        {
+            Console.WriteLine("Division = " + cal.Divide(a, b));
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero.");
+        }
+    }
+}
---
+++ 11-NUnit/CalculatorTests/CalculatorTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class CalculatorTest
+{
+    private Calculator1 cal;
+
+    [SetUp]
+    public void Setup()
+    {
+        cal = new Calculator1();
+    }
+
+    [Test]
+    public void TestAdd()
+    {
+        Assert.That(cal.Add(10, 5), Is.EqualTo(15));
+    }
+
+    [Test]
+    public void TestSubtract()
+    {
+       Assert.That(cal.Subtract(10, 5), Is.EqualTo(5));
+    }
+
+    [Test]
+    public void TestMultiply()
+    {
+       Assert.That(cal.Multiply(10, 5), Is.EqualTo(50));
+    }
+
+    [Test]
+    public void TestDivide()
+    {
+       Assert.That(cal.Divide(10, 5), Is.EqualTo(2));
+    }
+
+    [Test]
+    public void TestDivideByZero()
+    {
+        Assert.Throws<DivideByZeroException>(() => cal.Divide(10, 0));
+    }
+}
